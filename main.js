3bbd2a4bc27dfa7fe7189a8fce52aef3d89e951b
Sungmiii <christyksmile@gmail.com>
2018-09-06 16:27:46 +1200

    small change able to bakc only once

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 let entries = [];
 let total = 0;
+let pressNums ="";//number added not just once added 
 
-let temp ="";
+// function for click the button on the calculator
 $('button').click(function(){
-    console.log('you clicked', this.innerHTML)
-    let value = $(this).text();
-    if(!isNaN(value)||value==='.'){
-        temp += value;
-    $('#display').val(temp.substring(0,10));
-    } else if (value === 'AC'){
-        entries =[];
-        temp ="";
-        total =0;
+    //declare variable pressedbutton and equal to the buttonvalue.
+    let buttonClick = this.innerHTML
+    //checking which button I've clicked and it's correct
+    console.log('you clicked', buttonClick)
+    //show the value I clicked on the input #display 
+    let value = buttonClick.toString();// same as $(this).text() but not sure about $(this) as yet
+    if(!isNaN(value) || value === '.'){
+        pressNums += value;
+        $('#display').val(pressNums);
+    }else if ( value === 'AC'){
+        entries = [];
+        total = 0;
+        pressNums = "";
+        $('#display').val('');
+    }else if ( value === 'CE'){
+        $('#display').val(pressNums.substring(0,pressNums.length-1)); // minus 1 from string but still only one 
     }
+    console.log(value)
 });
 
