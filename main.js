80a060e486ca53087f7557e4e9e9dcc7d7d57cd9
Sungmiii <christyksmile@gmail.com>
2018-09-07 12:16:02 +1200

    sample reference too difficult change to onclick ver

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
-let entries = [];
-let total = 0;
-let pressNums ="";//number added not just once added 
+// declare input display id to use easily
+let display = $('#display')[0];
 
+//function add number into the display input and added continuely
+function insert(num) {
+    display.value = display.value + num
+};
 
-//load
-
-// function for click the button on the calculator
-$('button').click(function(){
-    //declare variable pressedbutton and equal to the buttonvalue.
-    let buttonClick = this.innerHTML
-    //checking which button I've clicked and it's correct
-    console.log('you clicked', buttonClick)
-    //show the value I clicked on the input #display 
-    let value = buttonClick.toString();// same as $(this).text() but not sure about $(this) as yet
-    if(!isNaN(value) || value === '.'){
-        pressNums += value;
-        $('#display').val(pressNums);
-    }else if ( value === 'AC'){
-        entries = [];
-        total = 0;
-        pressNums = "";
-        $('#display').val('');
-    }else if ( value === 'CE'){
-        $('#display').val(0,pressNums.substring(pressNums.length-1)); // minus 1 from string but still only one 
+//function operation from input value
+function total() {
+    let val = display.value;
+    if(val){
+        display.value = eval(val);
     }
-   
-         
-    console.log(value)// check what is my value is on console and is it correct ?
-   
+}
+
+// clear function
+$('#clear').click(function(){
+  $('#display').val('');
 });
+
+//back function
+$('#remove').click(function(){
+    let val = display.value;
+    display.value = val.substring(0,val.length-1);
+})
+
+
 
 
 
