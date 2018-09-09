512a5ab0f46ccabdadf35f381dc988de855676c9
Sungmiii <christyksmile@gmail.com>
2018-09-09 23:19:11 +1200

    refactor

--- main.js
+++ main.js
@@ -2,16 +2,24 @@
 let display = $('#display')[0];
 
 //function add number into the display input and added continuely
-function insert(num) {
+/* function insert(num) {
     display.value = display.value + num
-};
+}; */
 
-//function operation from input value
+// refactoring function without onclick in html
+$('button').click(function(){
+    let val = $('button').value;
+    if(!isNaN(val)){
+     $('#display').val(val.substring(0,10));
+    }
+})
+
+//function operation from input value, use eval: I read eval is not so great 
+//but I am not able to figure it out without eval func really want to give up this part
 function total() {
     let val = display.value;
-    if(val){
-        display.value = eval(val);
-    }
+    display.value = eval(val);
+    
 }
 
 // clear function
@@ -23,7 +31,7 @@ $('#clear').click(function(){
 $('#remove').click(function(){
     let val = display.value;
     display.value = val.substring(0,val.length-1);
-})
+});
 
 
 
